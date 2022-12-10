0b2916e599b0ef9c9cbad1d804ed2dde31d02ec5
yarburart <yaroslav.chayk@gmail.com>
2022-12-10 17:07:52 +0500

    add show

--- text2audio.py
+++ text2audio.py
@@ -2,8 +2,10 @@ from gtts import gTTS
 from playsound import playsound
 
 text = "Hello man, how are you?"
+def playtext(text):
+    obj = gTTS(text=text, lang='en', slow=False)
+    obj.save("text.mp3")
+    playsound("text.mp3")
 
-obj = gTTS(text=text, lang='en', slow=False)
-obj.save("text.mp3")
-playsound("text.mp3")
-
+if __name__ == '__main__':
+    playtext(text)
