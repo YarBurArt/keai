bbc68b90760d22692de2fa7f9a16bb53d8f0ffb9
yarburart <yaroslav.chayk@gmail.com>
2022-12-10 18:07:24 +0500

    debug

--- imgshow.py
+++ imgshow.py
@@ -1,14 +1,16 @@
 from tkinter import Tk, Label
 from PIL import ImageTk, Image
+import text2audio
 
-def show(image):
+def show(image, text):
     root = Tk()
     root.title("")
     image_no_1 = ImageTk.PhotoImage(Image.open(image))
     label = Label(image=image_no_1)
     label.grid(row=1, column=0, columnspan=3)
-    root.after(5000, root.destroy)
+    text2audio.playtext(text)
+    root.after(2000, root.destroy)
     root.mainloop()
 
 if __name__ == "__main__":
-    show("graphics/g2.png")
+    show("graphics/r1.png", "hello world")
