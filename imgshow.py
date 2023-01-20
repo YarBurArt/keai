e30f4c86deacad653e7659d295fe4754803d132e
YarBurArt <81087967+YarBurArt@users.noreply.github.com>
2023-01-20 12:28:12 +0500

    refactoring function show

--- imgshow.py
+++ imgshow.py
@@ -2,15 +2,21 @@ from tkinter import Tk, Label
 from PIL import ImageTk, Image
 import text2audio
 
-def show(image, text):
+
+def showimg_tk(image_path, text):
     root = Tk()
-    root.title("")
-    image_no_1 = ImageTk.PhotoImage(Image.open(image))
+    root.title("rei emotion")
+
+    loadedfile_img = Image.open(image_path)
+    image_no_1 = ImageTk.PhotoImage(loadedfile_img)
     label = Label(image=image_no_1)
     label.grid(row=1, column=0, columnspan=3)
+    
     text2audio.playtext(text)
+
     root.after(2000, root.destroy)
     root.mainloop()
 
+
 if __name__ == "__main__":
-    show("graphics/r1.png", "hello world")
+    showimg_tk("graphics/r1.png", "hello world")
