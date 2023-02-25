d65992f32df147881b5ca7bd59124dc2e1c26951
YarBurArt <yaroslav.chayk@gmail.com>
2023-02-25 22:08:17 +0500

    conect emo to img

--- imgshow.py
+++ imgshow.py
@@ -12,7 +12,7 @@ def showimg_tk(image_path, text):
     label = Label(image=image_no_1)
     label.grid(row=1, column=0, columnspan=3)
     
-    text2audio.playtext(text)
+    # text2audio.playtext(text)
 
     root.after(2000, root.destroy)
     root.mainloop()
