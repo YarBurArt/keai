5c768d8ae594166d8b22033300344f885994d7b3
Yaroslav <81087967+YarBurArt@users.noreply.github.com>
2023-02-28 08:28:39 +0500

    Refactoring code

--- emorec.py
+++ emorec.py
@@ -13,7 +13,8 @@ def init_emotion(model="models/emotion-ferplus-8.onnx"):
     global net, emotions
 
     # Define the emotions
-    emotions = ['Neutral', 'Happy', 'Surprise', 'Sad', 'Anger', 'Disgust', 'Fear', 'Contempt']
+    emotions = ['Neutral', 'Happy', 'Surprise', 
+                'Sad', 'Anger', 'Disgust', 'Fear', 'Contempt']
 
     # Initialize the DNN module
     net = cv2.dnn.readNetFromONNX(model)
@@ -32,7 +33,8 @@ def emotion(image, returndata=False):
     # Iterate process for all detected faces
     for x, y, w, h in faces:
         # Get the Face from image
-        face = img_copy[y - padding:y + h + padding, x - padding:x + w + padding]
+        face = img_copy[y - padding:y + h + padding, 
+                        x - padding:x + w + padding]
 
         # Convert the detected face from BGR to Gray scale
         gray = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
@@ -60,16 +62,21 @@ def emotion(image, returndata=False):
         predicted_emotion = emotions[prob.argmax()]
         print(predicted_emotion)
         if predicted_emotion == 'Happy':
-            imgshow.showimg_tk('graphics/r1.png', "it's good that you feel alive")
+            imgshow.showimg_tk('graphics/r1.png', 
+                               "it's good that you feel alive")
+        
         # Write predicted emotion on image
-        cv2.putText(img_copy, '{}'.format(predicted_emotion), (x, y + h + (1 * 20)), cv2.FONT_HERSHEY_SIMPLEX, 1,
+        cv2.putText(img_copy, '{}'.format(predicted_emotion), 
+                    (x, y + h + (1 * 20)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1,
                     (255, 0, 255),
                     2, cv2.LINE_AA)
         # Draw a rectangular box on the detected face
-        cv2.rectangle(img_copy, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv2.rectangle(img_copy, (x, y), 
+                     (x + w, y + h), 
+                     (0, 0, 255), 2)
 
     if returndata:
-
         # Return the the final image if return data is True
         return img_copy
 
@@ -83,6 +90,7 @@ def emotion(image, returndata=False):
 fps = 0
 init_emotion()
 cap = cv2.VideoCapture(0)
+
 # src = cv2.cuda_GpuMat()
 # If you want to use the webcam the pass 0
 # cap = cv2.VideoCapture(0)
@@ -95,27 +103,27 @@ while True:
 
         # src = cv2.cuda_GpuMat()
         # src.upload(frame)
-        #
-        # clahe = cv2.cuda.createCLAHE(clipLimit=5.0, tileGridSize=(8, 8))
+        # clahe = cv2.cuda.createCLAHE(clipLimit=5.0, 
+                                       tileGridSize=(8, 8))
         # dst = clahe.apply(src, cv2.cuda_Stream.Null())
-        #
         # frame = dst.download()
 
         if not ret:
-            break
+            break  # restart cam
 
-        image = cv2.flip(frame, 1)
+        image_pre = cv2.flip(frame, 1)
 
-        image = emotion(image, returndata=True)
+        image = emotion(image_pre, returndata=True)
 
-        cv2.putText(image, 'FPS: {:.2f}'.format(fps), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 20, 55), 1)
+        cv2.putText(image, 'FPS: {:.2f}'.format(fps),
+                   (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 
+                   0.8, (255, 20, 55), 1)
         cv2.imshow("Emotion Recognition", image)
 
         fps = (1.0 / (time.time() - start_time))
 
-        k = cv2.waitKey(1)
-        if k == ord('q'):
-            break
+        # k = cv2.waitKey(1)
+        # if k == ord('q'): break
 
 cap.release()
 cv2.destroyAllWindows()
