eca225e0b7888d567c3fafab01f83b11b89c66fd
Yaroslav <81087967+YarBurArt@users.noreply.github.com>
2023-03-06 09:00:32 +0500

    Update emorec.py

--- emorec.py
+++ emorec.py
@@ -26,10 +26,8 @@ def init_emotion(
 def emotion(image, returndata=False):
     # Make copy of  image
     img_copy = image.copy()
-
     # Detect faces in image
     faces = fd.ssd_detect(img_copy, conf=0.2)
-
     # Define padding for face ROI
     padding = 3
 
@@ -99,28 +97,23 @@ cap = cv2.VideoCapture(0)
         while True:
             time.sleep(0.2)  # the optimization fps and cpu resource
             start_time = time.time()
-            if not (ret, frame := cap.read()):
-                break
+            ret, frame = cap.read()
+          
 
-        # src = cv2.cuda_GpuMat()
-        # src.upload(frame)
-        #
+        # src = cv2.cuda_GpuMat().upload(frame)
         # clahe = cv2.cuda.createCLAHE(clipLimit=5.0, tileGridSize=(8, 8))
-        # dst = clahe.apply(src, cv2.cuda_Stream.Null())
-        #
-        # frame = dst.download()
+        # frame = clahe.apply(src, cv2.cuda_Stream.Null()).download()
 
         if not ret:
             break
 
         image = cv2.flip(frame, 1)
-
         image = emotion(image, returndata=True)
 
         cv2.putText(image, 'FPS: {:.2f}'.format(fps), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 20, 55), 1)
         cv2.imshow("Emotion Recognition", image)
 
-            fps = (1.0 / (time.time() - start_time))
+        fps = (1.0 / (time.time() - start_time))
 
         k = cv2.waitKey(1)
         if k == ord('q'):
