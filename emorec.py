b2029710ea6e583e518d0c671690a3c8ff0b1722
YarBurArt <yaroslav.chayk@gmail.com>
2023-03-10 14:57:16 +0500

    joy play

--- emorec.py
+++ emorec.py
@@ -88,36 +88,39 @@ cap = cv2.VideoCapture(0)
 # If you want to use the webcam the pass 0
 # cap = cv2.VideoCapture(0)
 
-    while True:
+while True:
+    try:
         while True:
             time.sleep(0.2)  # the optimization fps and cpu resource
             start_time = time.time()
-            if not (ret, frame := cap.read()):
+            ret, frame = cap.read()
+            if not ret:
                 break
 
-        # src = cv2.cuda_GpuMat()
-        # src.upload(frame)
-        #
-        # clahe = cv2.cuda.createCLAHE(clipLimit=5.0, tileGridSize=(8, 8))
-        # dst = clahe.apply(src, cv2.cuda_Stream.Null())
-        #
-        # frame = dst.download()
+            # src = cv2.cuda_GpuMat()
+            # src.upload(frame)
+            #
+            # clahe = cv2.cuda.createCLAHE(clipLimit=5.0, tileGridSize=(8, 8))
+            # dst = clahe.apply(src, cv2.cuda_Stream.Null())
+            #
+            # frame = dst.download()
 
-        if not ret:
-            break
 
-        image = cv2.flip(frame, 1)
 
-        image = emotion(image, returndata=True)
+            image = cv2.flip(frame, 1)
 
-        cv2.putText(image, 'FPS: {:.2f}'.format(fps), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 20, 55), 1)
-        cv2.imshow("Emotion Recognition", image)
+            image = emotion(image, returndata=True)
+
+            cv2.putText(image, 'FPS: {:.2f}'.format(fps), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 20, 55), 1)
+            cv2.imshow("Emotion Recognition", image)
 
             fps = (1.0 / (time.time() - start_time))
 
-        k = cv2.waitKey(1)
-        if k == ord('q'):
-            break
+            k = cv2.waitKey(1)
+            if k == ord('q'):
+                assert Exception("quit")
+    except Exception as ex:
+        pass
 
 cap.release()
 cv2.destroyAllWindows()
