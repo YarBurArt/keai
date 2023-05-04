4eaf86616a153fcbc75a2ca2236894f1806ef9ef
YarBurArt <yaroslav.chayk@gmail.com>
2023-05-04 20:21:36 +0500

    edit style

--- ipc.py
+++ ipc.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Copyright 2018-09-09, create by Wolfgang Kühn, edit by YarBurArt
+Copyright 2018-09-09 - 2023-05-05, create by Wolfgang Kühn, edit by YarBurArt
 Fast inter-process communication (ipc) for Python (CPython, PyPy, 2.7, 3.6+) on MS-Windows.
 It uses shared memory and eventing.
 Example rpc style synchronization:
@@ -20,14 +20,12 @@ Client (consumer, does not read data in this example):
         context.send_data_then_wait(message)
 A possible alternative is named pipes, see for example https://github.com/mark3982/pywpipe
 """
-import sys
-import mmap
+from sys import platform; assert platform == 'win32'
+from mmap import mmap, ACCESS_WRITE
 from ctypes import windll, WINFUNCTYPE
 import ctypes.wintypes as wt
-import logging
-from typing import Any
-
-assert sys.platform == 'win32'
+from logging import info as log_info
+from typing import Any, Union
 
 ##########################################################################
 # Minimal shim for win32event, in case you cannot have pywin32 (pypy, etc)
@@ -61,7 +59,7 @@ class Event:
     one event for two processes as this will interfere with the auto-reset nature.
     """
 
-    def __init__(self, name):
+    def __init__(self, name) -> None:
         """
         Creates or binds to an auto-reset event object, meaning the event
         is set to nonsignaled after the single waiting thread has been released.
@@ -70,10 +68,10 @@ class Event:
 
     def wait(self, timeout=None) -> bool:
         status = wait_for_single_object_release_gil(self._event,
-        INFINITE if timeout is None else timeout)
+                                                    INFINITE if None is timeout else timeout)
         return status == WAIT_OBJECT_0
 
-    def set(self):
+    def set(self) -> None:
         SetEvent(self._event)
 
 
@@ -87,23 +85,22 @@ class IpcContext:
         # Note 1: In Python 3.6.6 mmap.resize() is broken, so we cannot do dynamic allocation.
         # The length must be bigger than the biggest message send, ever!
         # Note 2: Initially, the buffer is initialized to all 0s. We use this to spot the first joining process.
-        self.shared_data = mmap.mmap(INFINITE, length=BUFFER_SIZE,
-                                     tagname='buffer_' + secret,
-                                     access=mmap.ACCESS_WRITE)
+        self.shared_data = mmap(INFINITE, length=BUFFER_SIZE,
+                                tagname='buffer_' + secret,
+                                access=ACCESS_WRITE)
 
         # Python 2.7 fix
         buffer = bytearray(self.shared_data[:])
 
-        activation_index = buffer[0]
-        if any(buffer[1:]) or activation_index not in {0, 1}:
-            raise AssertionError('The memory map is polluted. '
-                                 'Do you already have a process running?')
+        activation_index: int = buffer[0]
+        assert bool(any(buffer[1:]) or activation_index in {0, 1}), \
+            'The memory map is polluted. Do you already have a process running?'
 
         if activation_index == 0:
-            logging.info('my process was first')
-            self.shared_data[0:1] = b'\x01'  # Set first byte to 1 for second process.
+            log_info('my process was first')
+            self.shared_data[:1] = b'\x01'  # Set first byte to 1 for second process.
         else:
-            logging.info('my process was second')
+            log_info('my process was second')
             events.reverse()
 
         self.my_event, self.other_event = events
@@ -120,15 +117,14 @@ class IpcContext:
         self.other_event.set()
         return self.my_event.wait()  # NEVER set a breakpoint on this line else you will deadlock!
 
-    def send_data_then_wait(self, raw_doc: Any) -> bool:
+    def send_data_then_wait(self, raw_doc: Union[Any, str]) -> bool:
         self.send_data(raw_doc)
         return self.my_event.wait()  # NEVER set a breakpoint on this line else you will deadlock!
 
     def send_data(self, raw_doc: Any) -> None:
         # Copy raw_doc to memory map
         len_raw_doc = len(raw_doc)
-        if len_raw_doc > len(self.shared_data):
-            raise AssertionError('Message too big: {len_raw_doc}')
-        self.shared_data[0:len_raw_doc] = raw_doc
+        assert bool(len_raw_doc > len(self.shared_data)) is False, \
+            'Message too big: {len_raw_doc}'
+        self.shared_data[:len_raw_doc] = raw_doc
         self.other_event.set()
-
