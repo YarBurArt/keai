e94a2adacd44f60f019ff7a4b8360044429a4df9
YarBurArt <yaroslav.chayk@gmail.com>
2023-09-20 16:45:13 +0500

    new rei text generation

---
+++ utils/get_rei_text.py
@@ -0,0 +1,21 @@
+from playwright.sync_api import sync_playwright
+from playwright._impl._api_types import TimeoutError
+from chatKeai2 import run
+
+
+if __name__ == '__main__':
+    out_text = "hello"
+    with open('../data/rei_faketext.txt', 'a') as file:
+        for i in range(1000):
+            message = out_text
+            try:
+                with sync_playwright() as playwright:
+                    out_text = run(playwright, message)
+                    file.write(out_text)
+                    print(out_text)
+                    out_text = run(playwright, out_text,
+                                   chara_id="mWlPN3NU6GyyZKUiV93bfe_-LjIg4el7YY6BpUtpRlk")
+                    file.write(out_text)
+                    print(out_text)
+            except TimeoutError:
+                print("error: server timed out")
